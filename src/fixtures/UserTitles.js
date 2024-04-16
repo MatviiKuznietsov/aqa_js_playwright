4c986d7cf53e04c85b39855614401f345b8fdace
MTV <matveimtvcool@gmai.com>
2024-04-16 10:51:48 +0300

    HW_022 Playwright Network & API Request

---
+++ src/fixtures/UserTitles.js
@@ -0,0 +1,9 @@
+export const USER_MOCK_TITLES = {
+    "status": "ok",
+    "data": {
+        "userId": 112345,
+        "photoFilename": "default-user.png",
+        "name": "Jim",
+        "lastName": "Carrey"
+    }
+}
---
+++ tests/GaragrNetTest.spec.js
@@ -0,0 +1,17 @@
+import {expect, test} from "../src/fixtures/GarageFixtures.js"
+import {USER_MOCK_TITLES} from "../src/fixtures/UserTitles.js";
+
+
+test.describe("Garage Net tests", () => {
+    test("Substitute name and last name user on Garage page", async ({userGaragePage, page}) => {
+        await page.route('api/users/profile', async (route) => {
+            return route.fulfill({
+                status: 200,
+                body: JSON.stringify(USER_MOCK_TITLES)
+            })
+        })
+        await userGaragePage.profileNavLink.click()
+        await expect(userGaragePage.profileNameLable).toHaveText(`${USER_MOCK_TITLES.data.name} ${USER_MOCK_TITLES.data.lastName}`)
+        await page.pause()
+    })
+})
