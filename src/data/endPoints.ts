b7c1c60e80eae3dbc59ee59b60081c73a288f636
MTV <matveimtvcool@gmai.com>
2024-04-30 14:57:03 +0300

    HW_025 Github actions

--- src/data/endPoints.ts
+++ src/data/endPoints.ts
@@ -1,6 +1,6 @@
 export enum EndPoints {
-    SIGN_IN_END_POINT = "/api/auth/signin",
-    LOG_OUT_END_POINT = "/api/auth/logout",
+    SIGN_IN_END_POINT = "/api/auth/signin/",
+    LOG_OUT_END_POINT = "/api/auth/logout/",
     CAR_END_POINT = "/api/cars/",
     BRAND_END_POINT = "/api/cars/brands/",
     MODELS_END_POINT ="/api/cars/models/"
